c3b9d033ed4721b6d8589c73fcb37e6732eb4b18
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2018-06-14 11:40:11 +0300

    Добавлены исходные файлы IP-ядра Arria 10 Transceiver ATX PLL

---
+++ hdl/specific/arria10/a10_sata_xcvr_atxpll_core/a10_sata_xcvr_atxpll_core/synth/a10_sata_xcvr_atxpll_core_cfg.v
@@ -0,0 +1,5 @@
+config a10_sata_xcvr_atxpll_core_cfg;
+		design a10_sata_xcvr_atxpll_core;
+		instance a10_sata_xcvr_atxpll_core.xcvr_atx_pll_a10_0 use a10_sata_xcvr_atxpll_core_altera_xcvr_atx_pll_a10_171.a10_sata_xcvr_atxpll_core_altera_xcvr_atx_pll_a10_171_pm4bbmq;
+endconfig
+
